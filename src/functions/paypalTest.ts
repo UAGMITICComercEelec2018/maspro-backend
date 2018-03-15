e779dfe35b83e7edf64a5fa9c49d4e0dcd3b367f
CARLOS ROBERTO ALVAREZ MUÑOZ <ingcram@gmail.com>
2018-03-14 20:56:04 -0600

    sns compras y success y error addedd

--- src/functions/paypalTest.ts
+++ src/functions/paypalTest.ts
@@ -1,6 +1,7 @@
 "use strict";
 import { success, failure, notAllowed } from "./../libs/response-lib";
 import * as paypal from "paypal-rest-sdk";
+import { fail } from "assert";
 
 export async function webhook(event, context, callback) {
   console.log(paypal);
@@ -10,31 +11,61 @@ export async function webhook(event, context, callback) {
     client_secret: process.env.PAYPAL_CLIENT_SECRET
   });
 
+  const items = [
+    {
+      id: "item1",
+      description: "item1",
+      info: {
+        name: "item",
+        sku: "item",
+        price: "100.00",
+        currency: "USD",
+        quantity: 1
+      },
+      price: {
+        currency: "USD",
+        total: "100.00"
+      }
+    },
+    {
+      id: "item2",
+      description: "item2",
+      info: {
+        name: "item",
+        sku: "item",
+        price: "1000.00",
+        currency: "USD",
+        quantity: 1
+      },
+      price: {
+        currency: "USD",
+        total: "1000.00"
+      }
+    }
+  ];
+
+  const { itemID } = event.pathParameters;
+  const item = items.find(i => i.id == itemID);
+  if (!items) {
+    return callback(null, failure({ error: "Item no encontrado" }));
+  }
+
   var create_payment_json = {
     intent: "sale",
     payer: {
       payment_method: "paypal"
     },
     redirect_urls: {
-      return_url: "http://return.url",
-      cancel_url: "http://cancel.url"
+      return_url: process.env.PAYPAL_SERVICE_RETURN_URL,
+      cancel_url: process.env.PAYPAL_SERVICE_ERROR_URL
     },
     transactions: [
       {
         item_list: {
-          items: [
-            {
-              name: "item",
-              sku: "item",
-              price: "1.00",
-              currency: "USD",
-              quantity: 1
-            }
-          ]
+          items: [item.info]
         },
         amount: {
-          currency: "USD",
-          total: "1.00"
+          ...item
         },
         description: "This is the payment description."
       }
@@ -50,4 +81,32 @@ export async function webhook(event, context, callback) {
       return callback(null, success(payment));
     }
   });
+}
+
+export async function onPaypalResult(event, context, callback) {
+  console.log(event);
+  try {
+    const { successOrError } = event.pathParameters;
+    const { paymentId, PayerID: payer_id } = event.queryStringParameters;
+
+    if ("success" !== successOrError) {
+      throw event;
+    }
+    const payment = await new Promise((resolve, reject) =>
+      paypal.payment.execute(paymentId, { payer_id }, (error, payment) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(payment);
+      })
+    );
+  } catch (error) {
+    console.log(error);
+    return callback(null, failure(error));
+  }
+}
+
+export async function onPaypalPurchaseSNS(event, context, callback) {
+  console.log(event);
+  return callback(null, success({ event }));
 }
